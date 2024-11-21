1daadee9757bc40311219df2268398481bd9fd45
EtienneLm <112947657+EtienneLm@users.noreply.github.com>
2024-11-21 10:33:00 +0100

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = L.map("map").setView([20, 0], 2); 
+var map = L.map("map").setView([20, 0], 2);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -6,40 +6,44 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const locations = [
-  {
-    name: "Santorini",
-    coords: [36.3932, 25.4615],
-    type: "star",
-    imageUrl: "images/popup-images/popup-1.png", 
-  },
-  {
-    name: "Paris",
-    coords: [48.8566, 2.3522],
-    type: "star",
-    imageUrl: "images/popup-images/popup-1.png", 
-  },
-  {
-    name: "Canada",
-    coords: [56.1304, -106.3468],
-    type: "circle",
-    imageUrl: "",
-  },
-  {
-    name: "Switzerland",
-    coords: [46.8182, 8.2275],
-    type: "circle",
-    imageUrl: "",
-  },
-  { name: "Spain", coords: [40.4637, -3.7492], type: "circle", imageUrl: "" },
-  {
-    name: "La Réunion",
-    coords: [-21.1151, 55.5364],
-    type: "circle",
-    imageUrl: "",
-  },
+  { name: "Santorini", coords: [36.3932, 25.4615], type: "trophy" },
+  { name: "Normandie", coords: [49.1829, -0.3707], type: "trophy" },
+  { name: "Paris", coords: [48.8566, 2.3522], type: "trophy" },
+  { name: "Seville", coords: [37.3891, -5.9845], type: "pin-question" },
+  { name: "Rodez", coords: [44.35, 2.5667], type: "pin-question" },
+  { name: "Rome", coords: [41.9028, 12.4964], type: "pin-question" },
+  { name: "Venise", coords: [45.4408, 12.3155], type: "pin-question" },
+  { name: "Canada", coords: [56.1304, -106.3468], type: "pin-question" },
+  { name: "Switzerland", coords: [46.8182, 8.2275], type: "pin-question" },
 ];
 
-// Function to show the image popup
+const locationImages = {
+  Paris: "../images/popup-images/popup-1.png",
+  Venise: "../images/img-3.png",
+  Canada: "../images/img-2.png",
+};
+
+locations.forEach((location) => {
+  let iconClass;
+
+  if (location.type === "trophy") {
+    iconClass = "trophy";
+  } else if (location.type === "pin-question") {
+    iconClass = "pin-question";
+  }
+
+  const customIcon = L.divIcon({
+    className: iconClass,
+    iconSize: [20, 20],
+  });
+
+  const marker = L.marker(location.coords, { icon: customIcon }).addTo(map);
+
+  const imageSrc = locationImages[location.name] || "../images/default-image.png";
+
+  marker.on("click", () => showImagePopup(imageSrc));
+});
+
 function showImagePopup(imageUrl) {
   const popup = document.getElementById("image-popup");
   const imgElement = document.getElementById("popup-img");
@@ -47,31 +51,7 @@ function showImagePopup(imageUrl) {
   popup.style.display = "block";
 }
 
-// Function to create markers
-locations.forEach((location) => {
-  if (location.type === "star") {
-    // Create star marker
-    const starIcon = L.divIcon({
-      className: "star",
-      iconSize: [32, 32],
-    });
-    const marker = L.marker(location.coords, { icon: starIcon }).addTo(map);
-
-    // Add click event to star marker
-    marker.on("click", () => {
-      showImagePopup(location.imageUrl);
-    });
-  } else {
-    // Create circle marker
-    const circleIcon = L.divIcon({
-      className: "circle",
-      iconSize: [20, 20],
-    });
-    L.marker(location.coords, { icon: circleIcon }).addTo(map);
-  }
-});
-
-// Close image popup when clicked
-document.getElementById("image-popup").addEventListener("click", function () {
-  this.style.display = "none";
-});
+function closePopup() {
+  const popup = document.getElementById("image-popup");
+  popup.style.display = "none";
+}
